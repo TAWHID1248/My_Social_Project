f417ad966d4c4578e8feca27b87e9083c7c12a39
tawhid1248 <frzfardin2020@gmail.com>
2023-12-05 11:12:36 +0600

    home-page-10mins

--- App_Posts/views.py
+++ App_Posts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-
+from App_Posts.models import Post, User
 
 # Create your views here.
 
 
 @login_required
 def home(request):
-    return render(request, 'App_Posts/home.html', context={'title': 'Home'})
+    if request.method == 'GET':
+        search = request.GET.get('search', '')
+        result = User.objects.filter(username__icontains=search)
+    return render(request, 'App_Posts/home.html', context={'title': 'Home', 'search': search, 'result': result})
